8590c381f2232f036b7523ddac3f7cb18b555df3
Thomas Göttgens <tgoettgens@gmail.com>
2024-10-06 18:15:53 +0200

    remove debug code and v from directory name

--- .github/scripts/fetch_release.sh
+++ .github/scripts/fetch_release.sh
@@ -3,13 +3,13 @@ main() {
     echo "Parsing the repo list"
     while IFS= read -r repo; do
         if release=$(curl -fqs -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${ARTIFACTS_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/${repo}/releases); then
-            echo $release
             tag="$(echo "$release" | jq -r '.[0].tag_name')"
             zip_files="$(echo "$release" | jq -r '.[0].assets[] | select((.name | startswith("firmware-")) and (.name | endswith(".zip"))) | .name')"
             echo "Parsing repo $repo at $tag"
             if [ -n "$zip_files" ]; then
-                mkdir -p "firmware-$tag"
-                pushd "firmware-$tag" >/dev/null
+                xtag="${tag:1}"
+                mkdir -p "firmware-$xtag"
+                pushd "firmware-$xtag" >/dev/null
                 while IFS= read -r zip_file || [[ -n $zip_file ]]; do
                     echo "Getting ZIP ${zip_file}"
                     wget -q "https://github.com/${repo}/releases/download/${tag}/${zip_file}"
