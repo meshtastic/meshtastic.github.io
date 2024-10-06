cf9703888500b3cfaeaad812f4528ba81069af25
Thomas Göttgens <tgoettgens@gmail.com>
2024-10-06 16:03:27 +0200

    update token

--- .github/scripts/fetch_release.sh
+++ .github/scripts/fetch_release.sh
@@ -2,7 +2,7 @@
 main() {
     echo "Parsing the repo list"
     while IFS= read -r repo; do
-        if release=$(curl -fqs -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${secrets.ARTIFACTS_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/${repo}/releases); then
+        if release=$(curl -fqs -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${ARTIFACTS_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/${repo}/releases); then
             echo $release
             tag="$(echo "$release" | jq -r '.[0].tag_name')"
             zip_files="$(echo "$release" | jq -r '.[0].assets[] | select(.name | endswith(".zip")) | .name')"
